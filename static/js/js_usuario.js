1d216468fe2ac7b11825cc47ee1e3138b458423c
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-05-14 00:38:37 -0300

    Função abrir e fechar PopUp
    Esse arquivo permite o funcionamento dos popups

---
+++ static/js/js_usuario.js
@@ -0,0 +1,8 @@
+function abrir_popup(){
+  document.getElementById('popup_usuario').style.display = 'block'
+
+}
+
+function fechar_popup(){
+  document.getElementById('popup_usuario').style.display = 'none'
+}
