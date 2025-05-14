07976a6c99901085b7d7f7e2c253c8b9b88ab8a4
Yan Carlos <yan.c.oliveira08@gmail.com>
2025-05-14 00:37:07 -0300

    Conexão e Flask
    arquivo para a utilização de Flask e Mysql-Connector, hoje eu fiz a conexão com o banco, permiti conexão com o login(ainda sem distinção de cargo) nem todas as rotas estão funcionando mas a de login vai para menu e menu para gestão de usuarios.
    na parte de gestão de usuarios esta quase tudo pronto para a criação de usuarios, as listas estão organizadas e é possivel abrir um popup sem estilização alguma

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, session, redirect
+import mysql.connector
+
+app = Flask(__name__)
+app.secret_key = "banana"
+
+#conexão com o banco
+conn = mysql.connector.connect(
+    user="root",
+    host="localhost",
+    port="3306", #verificar a porta do senai para possiveis mudancas
+    password="root",
+    database="sa_padariamokeleymbembe"
+)
+cursor = conn.cursor()
+
+#flask em acao
+@app.route("/")
+def index():
+    return render_template("html/login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    
+    
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+
+    cursor.execute("select * from usuarios where nome=%s and senha=%s", (usuario, senha))
+    user = cursor.fetchone()
+
+    if user:
+        session['usuario'] = usuario
+        return redirect('/menu')
+        
+    else:
+        return render_template("html/login.html", erro_mensagem=True)
+
+@app.route("/menu")
+def menu():
+    return render_template("html/menu-gerente.html")
+
+@app.route("/gestao-usuarios")
+def gestao_usuarios():
+    cursor.execute("""select u.id_usuario, u.nome, u.cpf, u.email, c.descricao from usuarios as u join cargos as c 
+    on u.id_usuario = c.id_cargo""")
+    usuarios = cursor.fetchall()
+    return render_template("html/Gestao-usuario.html", usuarios=usuarios)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
